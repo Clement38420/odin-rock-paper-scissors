28c4cb4cef95892d543976c7cd62863c8a664e21
Clement Charbonnel <c.charbo38@gmail.com>
2024-08-17 00:06:08 +0200

    ADD THE FUNCTION TO PLAY A SINGLE ROUND
    And make getHumanChoice case-insensitive

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+let humanScore = 0
+let computerScore = 0
+
 function randomMove() {
     let randomNumber = Math.floor(Math.random() * 10) % 3
     switch (randomNumber) {
         case 0:
-            return "Rock"
+            return "rock"
         case 1:
-            return "Paper"
+            return "paper"
         case 2:
-            return "Scissors"
+            return "scissors"
     }
 }
 
 function getHumanChoice() {
-    return prompt("Please choose a move :")
+    return prompt("Please choose a move :").toLowerCase()
 }
+
+function playRound(computerChoice, humanChoice) {
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice == "rock") return "Ex-aequo"
+            else if (computerChoice == "scissors") return `You Win ${humanChoice} beats ${computerChoice}!`
+            else return `You Lose ${computerChoice} beats ${humanChoice}!`
+        case "scissors":
+            if (computerChoice == "scissors") return "Ex-aequo"
+            else if (computerChoice == "paper") return `You Win ${humanChoice} beats ${computerChoice}!`
+            else return `You Lose ${computerChoice} beats ${humanChoice}!`
+        case "paper":
+            if (computerChoice == "paper") return "Ex-aequo"
+            else if (computerChoice == "rock") return `You Win ${humanChoice} beats ${computerChoice}!`
+            else return `You Lose ${computerChoice} beats ${humanChoice}!`
+        default:
+            return "Please enter a valid move"
+    }
+}
+
+console.log(playRound(randomMove(), getHumanChoice()))
